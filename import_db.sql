8a70037fa1234a10100a770ed1ddc27062dcf494
Wojciech <klonowski.wojtek@yahoo.co.uk>
2023-12-23 16:55:50 +0100

    building DV

--- import_db.sql
+++ import_db.sql
@@ -1,5 +1,17 @@
 
-CREATE DATABASE users(
-    fname TEXT NOT NULL,
-    lname TEXT NOT NULL;
+CREATE TABLE users(
+    id INTEGER PRIMARY KEY
+    fname VARCHAR(255) NOT NULL,
+    lname VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE questions(
+    id INTEGER PRIMARY KEY
+    title VARCHAR(255) NOT NULL,
+    body VARCHAR(255) NOT NULL,
+    author_id INTEGER NOT NULL
+    
+
+    FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
