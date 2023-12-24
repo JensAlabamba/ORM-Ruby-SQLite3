1f73a8743a89878b0db9dc9382855defac7a2d51
Wojciech <klonowski.wojtek@yahoo.co.uk>
2023-12-24 20:25:28 +0100

    setting up user ORM

--- questions.rb
+++ questions.rb
@@ -1,27 +1,51 @@
 require 'sqlite3'
-require 'singletion'
+require 'singleton'
 
 class QuestionsDBConnection < SQLite3::Database
      include Singleton
 
      def initialize
-          super(aa_questions.db)
+          super("aa_questions.db")
           self.type_translation = true
           self.results_as_hash = true
      end
 end
 
 class User
-
+     attr_accessor :id, :fname, :lname
      def self.all
           data = QuestionsDBConnection.instance.execute("SELECT * FROM users;")
           data.map { |datum| User.new(datum) }
      end
 
-     def initialize[options]
+     def initialize(options)
           @id = options['id']
           @fname = options['fname']
           @lname = options['lname']
+     end
+
+     def create
+          raise "#{self} already in a database" if @id
+          QuestionsDBConnection.instance.execute(<<-SQL, self.fname, self.lname)
+          INSERT INTO 
+               users(fname, lname)
+          VALUES
+               (?, ?)
+          SQL
+          @id = QuestionsDBConnection.instance.last_insert_row_id
+     end
+
+     def update
+          raise "#{self} not in a database" unless @id
+          QuestionsDBConnection.instance.execute(<<-SQL, self.fname, self.lname, self.id)
+          UPDATE 
+               users
+          SET
+               fname = ?,
+               lname = ?
+          WHERE
+               id = ?
+          SQL
      end
 end
 
