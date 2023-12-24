88db829d53374f2b9448e4bafa013ca295c3ff40
Wojciech <klonowski.wojtek@yahoo.co.uk>
2023-12-24 12:59:11 +0100

    Table creation

--- import_db.sql
+++ import_db.sql
@@ -1,17 +1,56 @@
+PRAGMA foreign_keys = ON;
+DROP TABLE if EXISTS users; 
 
 CREATE TABLE users(
-    id INTEGER PRIMARY KEY
+    id INTEGER PRIMARY KEY,
     fname VARCHAR(255) NOT NULL,
     lname VARCHAR(255) NOT NULL
 );
 
+DROP TABLE if EXISTS questions;
+
 CREATE TABLE questions(
-    id INTEGER PRIMARY KEY
+    id INTEGER PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     body VARCHAR(255) NOT NULL,
-    author_id INTEGER NOT NULL
+    author_id INTEGER NOT NULL,
     
 
     FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+DROP TABLE if EXISTS question_follows;
+
+CREATE TABLE question_follows(
+    question_id INTEGER NOT NULL,
+    follower_id INTEGER NOT NULL,
+
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (follower_id) REFERENCES users(id)
+);
+
+DROP TABLE if EXISTS replies;
+
+CREATE TABLE replies(
+    id INTEGER PRIMARY KEY,
+    question_id INTEGER NOT NULL,
+    body VARCHAR(255) NOT NULL,
+    user_id INTEGER NOT NULL,
+    parent_reply INTEGER,
+
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (parent_reply) REFERENCES replies(id)
+);
+
+DROP TABLE if EXISTS question_likes;
+
+CREATE TABLE question_likes(
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+    liked BOOLEAN,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
 );
 
